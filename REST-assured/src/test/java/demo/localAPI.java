43cc7972ae5f38bcb5f91747fa6c175fd3e53ab3
Pritamh <pritam.horo143@gmail.com>
2024-12-04 09:19:07 +0530

    Creating a new user through POST using restAssured and fetching the new data

--- REST-assured/src/test/java/demo/localAPI.java
+++ REST-assured/src/test/java/demo/localAPI.java
@@ -1,5 +1,6 @@
 package demo;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -7,6 +8,7 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.nio.channels.AcceptPendingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,8 +17,42 @@ import org.testng.annotations.Test;
 
 public class localAPI 
 {
-	@Test
+	@Test(priority = 2)
 	public void get() 
+	{
+		baseURI="http://localhost:3000";
+		given().
+		when().get("/Users").
+		then().
+			statusCode(200).
+			log().body();
+	}
+	
+	
+	// Update with the new user 
+	
+	@Test(priority = 2)
+	public void post() 
+	{
+		JSONObject request=new JSONObject();
+		
+		request.put("FirstName", "JhON");
+		request.put("LasttName", "Cena");
+		request.put("SubjectID", 7);
+		
+		
+		
+		baseURI="http://localhost:3000";
+		given().contentType(ContentType.JSON).
+		accept(ContentType.JSON).
+		body(request.toJSONString()).
+		when().post("/Users").
+		then().
+			statusCode(201).
+			log().body();
+	}
+	@Test
+	public void get1() 
 	{
 		baseURI="http://localhost:3000";
 		given().
